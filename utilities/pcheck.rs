7926dc30142a676816dcba1c96d9f864cea62c39
Jiahong Long <jiahong888long@gmail.com>
2020-03-16 21:24:18 -0700

    Adding some documentation to the pcheck macro

--- utilities/pcheck.rs
+++ utilities/pcheck.rs
@@ -1,3 +1,11 @@
+/// Allows you to wrap syscalls such that failure of the syscall causes a panic and error dump.
+///
+/// Ex:
+/// `pcheck!(set_scheduler(0, SCHED_FIFO, priority))` 
+///
+/// Additional arguments can be provided; provided the first argument is a syscall, the second
+/// optional argument is custom error text, and the third optional argument is a custom error
+/// format (per format! specs). 
 #[macro_export]
 macro_rules! pcheck {
     ($function:expr) => {{
